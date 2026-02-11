73fb63afcd3be1f76ca901b264074d139f5bb979
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-02-11 10:19:03 +0100

    creat  container of time

---
+++ main.js
@@ -0,0 +1,51 @@
+// let input = document.getElementById('input');
+// let btn = document.getElementById('btn');
+
+// let delete_button = document.getElementById('delete');
+// let update_button = document.getElementById('update');
+
+
+
+// let array = [];
+// console.log(array);
+// btn.onclick = function(){
+    
+//           let objet = {
+//             tasks : input.value,
+//           }
+    
+//     array.push(objet);
+//      showdata();
+//      clearData();
+
+// }
+
+// function showdata(){
+//     let table = '';
+//         for(let i = 0 ; i< array.length ; i++){
+//             table += `
+//         <div id="tasks_content" class="text-[26px] font-semibold">${array[i].tasks}</div>
+        
+        
+//         `;
+//         }
+        
+    
+//      document.getElementById('tasks_content').innerHTML = table;
+// }
+// function clearData(){
+//     input.value = "";
+// }
+
+
+// delete_button.onclick = function(){
+//     array.splice(array[0] , 1);
+//     showdata();
+// }
+
+// let start_button = document.getElementById('start_button');
+// let stope_button = document.getElementById('stope_button');
+// let minute = document.getElementById('minute');
+// let second = document.getElementById('second');
+
+
